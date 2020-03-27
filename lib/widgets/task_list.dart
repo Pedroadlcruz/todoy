acd49095d3285eeec83d6337e2f2ab85d7696e85
pedroadlcruz <pedroadlcruz@gmail.com>
2020-03-26 21:06:49 -0400

    Check State

--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_flutter/models/task.dart';
 import 'package:todoey_flutter/widgets/task_tile.dart';
 
-class TaskList extends StatelessWidget {
+class TaskList extends StatefulWidget {
+
+  @override
+  _TaskListState createState() => _TaskListState();
+}
+class _TaskListState extends State<TaskList> {
+
+List<Task> tasks = [
+  Task(name: 'Buy Milk'),
+  Task(name: 'Buy Mango'),
+  Task(name: 'Buy Bread'),
+];
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ListView.builder(itemBuilder: (context, index){
+       return TaskTile(
+         taskTitle: tasks[index].name,
+         isChecked: tasks[index].isDone ,
+         checkboxCallback: (bool checkboxState){
+           setState(() {
+        tasks[index].toggleDone() ; 
+          }); 
+        },
+         );
+    },
+    itemCount: tasks.length,
     );
   }
 }
